86025e8cdfdadacae820bda2e7dd6d24e1c68c3f
chuckbradley <cb2036@att.com>
2015-03-08 04:22:25 -0400

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Chuck Bradley on 3/7/15.
+//  Copyright (c) 2015 FreedomMind. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
